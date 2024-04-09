d81ed6fd3efbf771d66e868529905a73b6511193
Tran Ly Nhat Hao <118340413+tranlynhathao@users.noreply.github.com>
2024-04-09 20:00:23 +0700

    Delete

--- Source/Redo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Struct.h"
-
-// Linked List Function to Redo
-Node* createNode(char** temp);
-void addHead(Node*& pHead, char** temp);
-void removeHead(Node*& pHead);
-void removeAll(Node*& pHead);
-void redo(Node* pHead, char** a);
